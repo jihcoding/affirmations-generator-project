bbd241bfb1caf2729e438f1bafbdd36c579ac736
jihcoding <jihcoding@gmail.com>
2025-10-10 22:02:49 +0200

    Improved users experience

--- src/script.js
+++ src/script.js
@@ -10,18 +10,17 @@ function displayAffirmation(response) {
 function generateAffirmations(event) {
   event.preventDefault();
 
-  id = "user-instructions";
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "682o04ta365f8dfdebf99e4a809fea32";
-  let prompt = `Generate a lovely affirmations ${instructionsInput.value}`;
-  let context = `User instructions: You are an affirmation generator AI, that generates short, calming affirmations to uplift the user's mood. 
-    Keep it under 20 words, warm tone, human-like and emotionally authentic. Include 1-2 emojis in the end of affirmation text that naturally fit the tone of the affirmation. 
-    End the text with 'Shecodes AI' preceded by an "em" and "strong" element.`;
+  let prompt = `You are an affirmation generator AI. The user gives you an idea, advice or emotion: ${instructionsInput.value}`;
+  let context = `User instructions: You are an affirmation generator AI. Keep it at least 30 words, warm tone, human-like and emotionally authentic. Include 1-2 emojis in the end of affirmation text that naturally fit the tone of the affirmation. After the text end it with 
+  '- Shecodes AI' preceded by an "em" and "strong" element and separate each line using HTML tag "br".`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating affirmations");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let affirmationElement = document.querySelector("#affirmations-text");
+  affirmationElement.classList.remove("hidden");
+  affirmationElement.innerHTML = `<div class="generating">⏳ Generating an affirmation about ${instructionsInput.value}</div>`;
+
 
   axios.get(apiURL).then(displayAffirmation);
 }
